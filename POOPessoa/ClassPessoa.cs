e4f4d0ca17913fdb093981dc20280427b7627c68
Luisa Jacob <ljacobpoveda@gmail.com>
2023-09-25 14:47:45 -0300

    Criando classe pessoa com C#

---
+++ POOPessoa/ClassPessoa.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace POOPessoa
+{
+    internal class Pessoa
+    {
+        //Definição de atributos da Classe Pessoa
+        public string nome { get; set; }
+        public int idade { get; set; }
+
+        //Método para exibir o nome e idade da pessoa
+        public void ExibirDados()
+        {
+            Console.WriteLine("Olá, " + nome + ", você tem " + idade + " anos.");
+        }
+    }
+}
---
+++ POOPessoa/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace POOPessoa
+{
+    
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //instanciando, creiando objeto
+            Pessoa obj = new Pessoa();
+
+            //solicitando ao usuário o nome
+            Console.WriteLine("Digite seu nome: ");
+            obj.nome= Console.ReadLine();
+
+            //solicitando aqo usuáriol a idade
+            Console.WriteLine("Digite sua idade: ");
+            obj.idade = int.Parse(Console.ReadLine());
+
+            obj.ExibirDados();
+
+            Console.ReadKey();
+        }
+    }
+}
